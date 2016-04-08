618d4231ebeeffe8fd2fd6f313e56e7dacd7a3d3
Matthew Gilman <gilmam@rpi.edu>
2016-04-08 12:49:10 -0400

    new stuff

--- Lab8.R
+++ Lab8.R
@@ -1,27 +1,14 @@
 source("http://bioconductor.org/biocLite.R")
 biocLite("Rgraphviz")
 
-str(Admissions)
-df <- as.data.frame(Admissions)
-head(df)
-Admissions.raw <- NULL
-for(i in 1:4) {
-  Admissions.raw <- cbind(Admissions.raw, rep(as.character(df[,i]), df$Freq))
-}
-Admissions.raw <- as.data.frame(Admissions.raw)
-names(Admissions.raw) <- names(df)[1:4]
-dim(Admissions.raw)
-str(Admissions.raw)
-library(arules)
-rules.all <-apriori(Admissions.raw)
-inspect(rules.all)
+col_names <- head(Admissions)
 
-for(i in 1:4){
-  Admissions.raw <- cbind(Admissions.raw, rep(as.character(df[,i]), df$rank))
-}
 
-Admissions.raw <- NULL
-for(i in 1:4){
-  Admissions.raw <- cbind(Admissions.raw, rep(as.character(df[,i]), df$gpa))
-}
+library(arulesViz)
 
+rules = apriori(col_names, parameter=list(support=0.08, confidence=0.9))
+inspect(rules)
+
+plot(rules)
+plot(rules, method="graph", control=list(type="items"))
+plot(rules, method="paracoord", control=list(reorder=TRUE))
